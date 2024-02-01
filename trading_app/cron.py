f95573da6f66ad18b8900f49af80a1c93e2e69c4
Dainis Berzins <db5331@gmail.com>
2024-02-01 21:30:25 +0200

    Restored strategy testing to all stocks

--- trading_app/cron.py
+++ trading_app/cron.py
@@ -39,7 +39,7 @@ class DailyPriceDownloadCronJob(CronJobBase):
 
 class DailyUSPriceDownloadCronJob(CronJobBase):
     RUN_AT_TIMES = ['00:01']  # Run at 23:00 local time
-    #schedule = Schedule(run_at_times=RUN_AT_TIMES)
+    schedule = Schedule(run_at_times=RUN_AT_TIMES)
     #schedule = Schedule(run_every_mins=3)  # Run once a day
     code = 'trading_app.daily_us_price_download_cron_job'
 
