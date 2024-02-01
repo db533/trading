4d178d43ad5aaf251565bc994b91655b295efb3c
Dainis Berzins <db5331@gmail.com>
2024-02-01 16:35:08 +0200

    Gann point 4. Debugging without Time comparison v0.8

--- trading_app/price_download.py
+++ trading_app/price_download.py
@@ -600,7 +600,7 @@ def identify_highs_lows_gann(df, reversal_days=2, price_move_percent=1.5):
                 for x in range(0, reversal_days):
                     if window_slice.iloc[x]['Low'] > window_slice.iloc[x+1]['Low']:
                         count_consequetive += 1
-                print(index_label,' count_consequetive:',count_consequetive)
+                #print(index_label,' count_consequetive:',count_consequetive)
                 if count_consequetive == reversal_days:
                     # The subsequent day(s) have had lows lower than on Day 0.
                     # Up trend ended on Day 0.
