f6b52bc18912be287e76413643de8dbc1af3d33c
Dainis Berzins <db5331@gmail.com>
2024-02-06 16:31:10 +0200

    Amended Gain 4 buy to use SwingPoint model v0.2

--- trading_app/price_download.py
+++ trading_app/price_download.py
@@ -544,6 +544,7 @@ def identify_highs_lows_gann(ticker, df, reversal_days=2, price_move_percent=1.5
     print('len(df):', len(df))
 
     # Delete existing swing points for the ticker as new ones will be determined.
+    print('ticker:', ticker)
     SwingPoint.objects.filter(ticker=ticker).delete()
     swing_point_id_list = []
 
