c2530958fe21e2e40586bad938ff14f6ecefe19a
Dainis Berzins <db5331@gmail.com>
2024-02-08 10:39:42 +0200

    Added a new 3 min cron job to action some tasks for limited tickers in Test tickers.

--- trading_app/cron.py
+++ trading_app/cron.py
@@ -39,12 +39,22 @@ class DailyPriceDownloadCronJob(CronJobBase):
 
 class DailyUSPriceDownloadCronJob(CronJobBase):
     RUN_AT_TIMES = ['00:01']  # Run at 23:00 local time
-    #schedule = Schedule(run_at_times=RUN_AT_TIMES)
-    schedule = Schedule(run_every_mins=3)  # Run once a day
+    schedule = Schedule(run_at_times=RUN_AT_TIMES)
+    #schedule = Schedule(run_every_mins=3)  # Run once a day
     code = 'trading_app.daily_us_price_download_cron_job'
 
     def do(self):
         category_price_download('US stocks')
+
+class TestPriceDownloadCronJob(CronJobBase):
+    RUN_AT_TIMES = ['00:01']  # Run at 23:00 local time
+    #schedule = Schedule(run_at_times=RUN_AT_TIMES)
+    schedule = Schedule(run_every_mins=3)  # Run once a day
+    code = 'trading_app.test_cron_job'
+
+    def do(self):
+        category_price_download('Test tickers')
+        process_trading_opportunities()
 
 
 class DailyTSEPriceDownloadCronJob(CronJobBase):
