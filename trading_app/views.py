1f38e363b9df0bdc292aff7f79b849ac988ba3ab
Dainis Berzins <db5331@gmail.com>
2024-02-09 15:19:39 +0200

    Fixing t=x for most recent price. v1.0

--- trading_app/views.py
+++ trading_app/views.py
@@ -917,6 +917,8 @@ import io
 def generate_swing_point_graph_view(request, opp_id):
     # Fetch the TradingOpp instance by ID
     opp = TradingOpp.objects.get(id=opp_id)
+    ticker = TradingOpp.ticker
+    print('Creating graph for ticker',ticker.symbol)
     # Access the metrics_snapshot directly
     metrics_snapshot = opp.metrics_snapshot
     #print('metrics_snapshot:',metrics_snapshot)
