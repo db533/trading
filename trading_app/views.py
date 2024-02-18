c37c0bafdf22c0e313fc200a53ea94013334f116
Dainis Berzins <db5331@gmail.com>
2024-02-18 15:23:36 +0200

    Adding duration since latest SP for Gann Buy eight customizer v0.3

--- trading_app/views.py
+++ trading_app/views.py
@@ -1472,7 +1472,7 @@ class GannEightCustomizer(BaseGraphCustomizer):
         # Draw a line from most recent sp up to HH level. Add time to most recent price.
         self.draw_vertical_line(ax, date_list[-1], price_list[-1], float(most_recent_price))
         self.draw_vertical_line(ax, most_recent_date, float(most_recent_price), price_list[-1])
-        label_price = price_list[-1] + offset_up
+        label_price = float(price_list[-1]) + offset_up
         label_date = date_list[-1] + (most_recent_date - date_list[-1]) / 2
         ax.text(label_date, label_price, f"t={round(duration_after_latest_sp, 0)}", fontsize=9, ha='center',
                 va='bottom')
