13afa7a65f27d96fc221a6eeb564d862e723d161
Dainis Berzins <db5331@gmail.com>
2024-03-31 21:56:48 +0300

    Amend Gann #1 to only be valid if retracement is greater than 80%

--- trading_app/update_strategies.py
+++ trading_app/update_strategies.py
@@ -194,7 +194,7 @@ class GannPointOneBuy(BaseStrategy):
                 retracement_as_percent = price_retracement * 100 / (peak_sp_price - prior_hl_price)
                 rise_after_retracement = latest_price.close_price - last_sp_price
                 rise_after_retracement_percent_of_retracement = rise_after_retracement * 100 / (-price_retracement)
-                if rise_after_retracement / -price_retracement < 0.5:
+                if rise_after_retracement / -price_retracement < 0.20:
                     data = {'sp_price_diff_vs_prior_high': str(sp_price_diff_vs_prior_high), 'price_retracement': str(price_retracement),
                             'retracement_as_percent': str(round(retracement_as_percent,1)), 'elapsed__duration': str(elapsed__duration),
                             'rise_after_retracement' : str(rise_after_retracement), 'recent_swing_points' : recent_swing_points,
@@ -280,7 +280,7 @@ class GannPointOneSell(BaseStrategy):
                 retracement_as_percent = price_retracement * 100 / (prior_lh_price - trough_sp_price)
                 fall_after_retracement = latest_price.close_price - last_sp_price
                 fall_after_retracement_percent_of_retracement = fall_after_retracement * 100 / (-price_retracement)
-                if fall_after_retracement / -price_retracement < 0.5:
+                if fall_after_retracement / -price_retracement < 0.20:
                     data = {'sp_price_diff_vs_prior_low': str(sp_price_diff_vs_prior_low), 'price_retracement': str(price_retracement),
                             'retracement_as_percent': str(round(retracement_as_percent,1)), 'elapsed__duration': str(elapsed__duration),
                             'fall_after_retracement' : str(fall_after_retracement), 'recent_swing_points' : recent_swing_points,  # recent_swing_points not as a string as it gets removed and accessed if present.
