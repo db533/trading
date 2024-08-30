2bdc560447142fcca64abbbfd63ca4abb91da605
Dainis Berzins <db5331@gmail.com>
2024-08-30 10:59:05 +0300

    Adding view to edit categories for Tickers. v1.7

--- trading_app/views.py
+++ trading_app/views.py
@@ -2828,13 +2828,12 @@ def manage_ticker_categories(request, category_id=None):
             for form in formset:
                 ticker = form.save(commit=False)  # Save the ticker instance without committing to DB yet
                 form.save_m2m()  # Save the many-to-many data
-                print(f"Saving Ticker: {ticker.symbol}")  # Debug: Confirm Ticker being saved
-                print(f"Categories: {form.cleaned_data['categories']}")  # Debug: Print selected categories
+                logger.info(f"Saving Ticker: {ticker.symbol}")
+                logger.info(f"Categories: {form.cleaned_data['categories']}")
                 ticker.save()  # Save the ticker instance with updated categories
             return redirect(request.path_info)
         else:
-            print("Formset is not valid:")
-            print(formset.errors)  # Debugging: print form errors to console/log
+            logger.error(f"Formset is not valid: {formset.errors}")
     else:
         formset = TickerFormSet(queryset=page_obj.object_list)
 
